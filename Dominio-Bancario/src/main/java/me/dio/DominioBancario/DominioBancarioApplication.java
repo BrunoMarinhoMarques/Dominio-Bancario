ff1e083a48959ed05280279cfbb9c8e32059ccf3
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-10-20 14:39:00 -0300

    Bugfix CORS

--- Dominio-Bancario/src/main/java/me/dio/DominioBancario/DominioBancarioApplication.java
+++ Dominio-Bancario/src/main/java/me/dio/DominioBancario/DominioBancarioApplication.java
@@ -3,6 +3,7 @@ package me.dio.DominioBancario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server")})
 @SpringBootApplication
 public class DominioBancarioApplication {
 
